6160fd71c1b0bac2a0fb7ea326a875b7fee03e64
Pedro Icaro <josepedroicaro@gmail.com>
2026-01-30 13:38:32 -0300

    Adjust

--- sistema.py
+++ sistema.py
@@ -1,9 +1,19 @@
 import pandas as pd 
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv") 
 
-print(df.head())
+df.head()
 
-print(df.info())
+df.info()
 
-print(df.describe())
+df.describe()
+
+plt.figure(figsize= (8,5))
+sns.barplot(data= df, x = 'experience_level', y = 'salary_in_usd')
+plt.title('Salário Médio Por Senioridade')
+plt.xlabel("Senioridade")
+plt.ylabel("Salario Médio Anual (USD)")
+plt.show()
